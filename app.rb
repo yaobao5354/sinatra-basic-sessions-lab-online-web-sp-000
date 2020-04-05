d54bd704cb6ceaa4d2718bb95f5f4f974668b439
yaoyu bao <yaobao5354@gmail.com>
2020-04-05 17:46:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
 
   configure do
@@ -9,6 +9,10 @@ class App < Sinatra::Base
 
   get '/' do
     erb :index
-  end 
+  end
 
+  post '/checkout' do
+    
+    binding.pry
+  end
 end
